a892532812c29dd4e23d85d718829930a10a86be
szheng <sharon.zheng@live.com>
2017-05-16 23:03:35 -0700

    update gitignore file

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var facts = require('./facts.json');
+
 var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
-  res.status(200).send('Hello WOrld');
+  res.status(200).send(facts[1] + ' Hello WOrldd ' + facts);
 });
 
 app.listen(port, function() {
